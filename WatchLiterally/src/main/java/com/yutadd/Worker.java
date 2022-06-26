31d6777077f7e375516e6937bdc27f74501d7a8a
yutadd <spyk4m49@gmail.com>
2022-06-26 12:40:39 +0900

    フリーのサービスとしてなら一応成り立つレベル

---
+++ WatchLiterally/src/main/java/com/yutadd/Worker.java
@@ -0,0 +1,47 @@
+package com.yutadd;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.TreeMap;
+
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FFmpegFrameGrabber.Exception;
+
+public class Worker {
+	static int number;
+	TreeMap<String,String> videos=new TreeMap<String,String>();
+	Path f = Paths.get("C:\\lives\\test\\");
+	public Worker(){
+		number++;
+		System.out.println(number);
+		Thread th=new Thread() {
+			@Override
+			public void run() {
+				while(true) {
+					for (File f2 : f.toFile().listFiles()) {
+						if (f2.isFile() && f2.getName().endsWith(".ts")) {
+							if(!videos.containsKey(f2.getName())) {
+								FFmpegFrameGrabber mInputGrabber = new FFmpegFrameGrabber(f2);
+								try {mInputGrabber.start();} catch (Exception e) {e.printStackTrace();}
+								String m3u8="";
+								m3u8 +="#EXTINF:"+(mInputGrabber.getLengthInFrames()/(float)mInputGrabber.getFrameRate())+", title\r\n";
+								m3u8 += "lives/test/" +f2.getName() + "\r\n";
+								videos.put(f2.getName(), m3u8);
+								try {mInputGrabber.close();} catch (org.bytedeco.javacv.FrameGrabber.Exception e) {e.printStackTrace();}
+							}
+						}
+					}
+
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						// TODO 自動生成された catch ブロック
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		th.start();
+	}
+}
--- WatchLiterally/src/main/resources/static/main.js
+++ WatchLiterally/src/main/resources/static/main.js
@@ -1,15 +1,15 @@
-$(function(){
+$(function () {
 	$.ajax({
-		type:"GET",
-		url:"/streamsdata",
-		datatype:"json"
-	}).done(function(data){
+		type: "GET",
+		url: "/streamsdata",
+		datatype: "json"
+	}).done(function (data) {
 		console.log(data);
-		var jdata=JSON.parse(data);
-		for(var i=0;i<jdata.length;i++){
-		$(".sidebar").append("<div class=\"side_elems\"><div class=\"side_elems_in\"><img class=\"icon\" width=\"40\" alt=\"icon\" src=\" " + jdata[i]["icon"]+"\"><div class=\"side_title_space\"><span class=\"side_title\">"+jdata[i]["name"]+"</span><br><span>"+jdata[i]["title"]+"</span></div></div></div>");
-	}
+		var jdata = JSON.parse(data);
+		for (var i = 0; i < jdata.length; i++) {
+			$(".sidebar").append("<div class=\"side_elems\"><div class=\"side_elems_in\"><img class=\"icon\" width=\"40\" alt=\"icon\" src=\" " + jdata[i]["icon"] + "\"><div class=\"side_title_space\"><span class=\"side_title\">" + jdata[i]["name"] + "</span><br><span>" + jdata[i]["title"] + "</span></div></div></div>");
+		}
 	}
 	);
-	
+
 });
